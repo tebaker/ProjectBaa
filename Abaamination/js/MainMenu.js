469e0d9c3177447b649e01eb6eea52b9158709eb
Bropocalypse <jalafond@ucsc.edu>
2017-05-06 23:58:34 -0700

    Core functioning prototype
    still needs Luke's player prefab so there will be some redundancy in Play.js once it's in
    also need to add Player.js to index.html once it's in

---
+++ Abaamination/js/MainMenu.js
@@ -0,0 +1,16 @@
+var MainMenu = function(game) {};
+MainMenu.prototype =
+{
+	preload: function()
+	{
+	},
+
+	create: function()
+	{
+	this.game.state.start('Play')
+	},
+
+	update: function()
+	{
+	},
+};
