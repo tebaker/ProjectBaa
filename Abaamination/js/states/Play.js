865e61508a6a38698cebe85b96ce639c3757d139
Talon Baker <taalonn@gmail.com>
2017-05-16 16:33:48 -0700

    combining the P2_Physics_Rework branch

--- Abaamination/js/states/Play.js
+++ Abaamination/js/states/Play.js
@@ -1,34 +1,68 @@
-'use strict';
+//object to hold the KeyCode properties
+function Buttons(up, down, left, right, jump, defend, ram){
+	this.up = up;
+	this.down = down;
+	this.right = right;
+	this.left = left;
+	this.jump = jump;
+	this.ram = ram;
+	this.defend = defend;
+};
+//object to hold collision groups for easy referencing
+function CollisionGroups(pCG, eCG, rCG, tCG){
+	this.playerCollisionGroup = pCG;
+	this.enemyCollisionGroup = eCG;
+	this.resourceCollisionGroup = rCG;
+	this.tilesCollisionGroup = tCG;
+};
+
+//'Play' state constructor
 var Play = function(game)
 {
 	this.cols = null;
+
+	//Collision bitmasks
+	this.pCG;	//playerCollisionGroup
+	this.tCG;	//tilesCollisionGroup
+	this.rCG;	//resourceCollisionGroup
+	this.eCG;	//enemyCollisionGroup
+
+	//object to hold all collision groups
+	this.collisionGroups;	
 };
+
 Play.prototype =
 {
 	preload: function() 
 	{
-		// enable FPS monitoring
-		if (debug) {
-		  game.time.advancedTiming = true;
-		}
 	},
 
 	create: function ()
 	{
-		//start physics
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-		game.physics.arcade.gravity.y = 2500;
-		game.physics.arcade.OVERLAP_BIAS = 30;
+		//Set input properties: Could create menu where User can change input mapping
+		var buttons = new Buttons(Phaser.KeyCode.UP, Phaser.KeyCode.DOWN, 
+			Phaser.KeyCode.LEFT, Phaser.KeyCode.RIGHT, Phaser.KeyCode.W, 
+			Phaser.KeyCode.Q, Phaser.KeyCode.E);
 
-		//mock background is this big
-		game.world.setBounds(0,0,3000,2080)
+		//Game World
+		game.world.setBounds(0,0,3000,2080);				//Set Background size
+		var mock = game.add.image(0, 0, 'mock');			//create background
 
-		//create background
-		var mock = game.add.image(0, 0, 'mock');
+		//Physics
+		game.physics.startSystem(Phaser.Physics.P2JS);		//Physics ignition
+		game.physics.p2.gravity.y = 2500;					//World gravity
 
-		//create invisible collider group
+		//Create collision groups
+		this.pCG = this.physics.p2.createCollisionGroup();
+		this.tCG = this.physics.p2.createCollisionGroup();
+		this.eCG = this.physics.p2.createCollisionGroup();
+		this.rCG = this.physics.p2.createCollisionGroup();
+		this.collisionGroups = new CollisionGroups(this.pCG, this.eCG, this.rCG, this.tCG);
+
+		//Create group of environmental colliders
 		this.cols = game.add.group();
 		this.cols.enableBody = true;
+		this.cols.physicsBodyType = Phaser.Physics.P2JS;
 
 		// . . . THIS IS WHY YOU USE A TILEMAP >_<!!!
 		//if u try to make one giant map image for some reason it doesnt work. needs singular pieces
@@ -57,46 +91,26 @@ Play.prototype =
 		col4.renderable = false;
 		col4.body.immovable = true;
 		col4.body.allowGravity = false;
+		//just experimentation: all of this collision data will be replaced with tile data
+		this.cols.forEach(function(shape){
+			shape.body.setCollisionGroup(this.tCG);
+		}, this, true, this.tCG);
 
-		//add player
-		player = game.add.sprite(300, 1330, 'player');
-		game.physics.arcade.enable(player);
-		player.body.bounce.y = 0.2;
-		player.body.collideWorldBounds = false;
+		//Player properties: game, x, y, key, frame, buttons, collisionGroup
+		player = new Player(this.game, 300, 1330, 'player', 0, buttons, this.collisionGroups);			
+		//Array of Collision groups to define player collisions
+		player.body.collides([	
+			this.collisionGroups.tilesCollisionGroup, 
+			this.collisionGroups.resourceCollisionGroup, 
+			this.collisionGroups.enemyCollisionGroup	
+		]);		 
 
-		//temporary movement
-		cursors = game.input.keyboard.createCursorKeys();
-		//basic camera no adjustments
-		game.camera.follow(player);
 		game.camera.deadzone = new Phaser.Rectangle(100, 100, 300, 20);
 	},
 
 	update: function()
 	{
-		var hitCol = game.physics.arcade.collide(player, this.cols);
-		player.body.velocity.x = 0;
 		
-		if(cursors.left.isDown)
-		{
-			player.body.velocity.x -= 500;
-
-		}
-
-		if(cursors.right.isDown)
-		{
-			player.body.velocity.x += 500;
-		}
-
-		//  Allow the player to jump if they are touching the ground.
-			if (cursors.up.isDown && hitCol)
-			{
-					player.body.velocity.y = -1100;
-			}
-
-			if (player.body.y > game.world.height)
-			{
-					game.state.start("Play");
-			}	
 	},
 
 	render: function()
@@ -104,7 +118,6 @@ Play.prototype =
 		if (debug) {
 			game.debug.cameraInfo(game.camera, 32, 32);
 			game.debug.spriteCoords(player, 900, 32);
-			game.debug.text('FPS: ' + game.time.fps, 20, 580, 'yellow');
 		}
 	},
 };
