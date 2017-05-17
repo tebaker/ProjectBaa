6750767374900603327965d2d8db112349919eef
Lucas Hackman <lhackman@ucsc.edu>
2017-05-17 02:22:49 -0700

    added polygon collision for player/ added function to scale polygon

--- Abaamination/js/player.js
+++ Abaamination/js/player.js
@@ -48,11 +48,17 @@ function Player(game, x, y, key, frame, buttonObj, cg){
 	this.body.collideWorldBounds = true;
 	this.body.restitution = 0.5;										//collision restitution
 
-
 	//Input mapping
 	buttons = game.input.keyboard.addKeys(buttonObj);					//Sets all the input keys for this prototype
 	buttons.ram.onDown.add(ram, this);									//captures the first frame of the jumpKey press event
 	buttons.jump.onDown.add(jump, this);								//captures the first frame of the ramKey press event
+	
+	//set button callbacks to 
+	buttons.right.onDown.add(startRun, this);
+	buttons.right.onUp(stopRun, this);
+	buttons.left.onDown.add(startRun, this);
+	buttons.left.onUp.add(stopRun, this)
+
 
 	game.camera.follow(this);											//attach the camera to the player
 	game.add.existing(this);											//add this Sprite prefab to the game cache
@@ -69,6 +75,8 @@ Player.prototype.update = function(){
 	//if the Player is not in the middle of a jump, gravity is applied to this body
 	if( !isJumping ) this.body.velocity.y = gravity * this.body.mass;
 
+	//if the user is pressing right and left at the same time, stop movement
+	if(buttons.left.isDown && buttons.right.isDown) this.body.velocity.x = 0;
 	//Movement
 	if(buttons.left.isDown){
 		this.body.moveLeft(moveSpeed / this.body.mass);
@@ -83,21 +91,39 @@ Player.prototype.update = function(){
 	}
 
 }
+//Resize a polygon Json file.  The polygon needs to be resized before it is applied to the body and 
+//the string associated with newPhysicsKey must be used. 
 function resizePolygon(originalPhysicsKey, newPhysicsKey, shapeKey, scale){
-	var newData = [];
-	var data = this.game.cache.getPhysicsData(originalPhysicsKey, shapeKey);
-	for (var i = 0; i < data.length; i++) {
-		var vertices = [];
-		for (var j = 0; j < data[i].shape.length; j += 2) {
-			vertices[j] = data[i].shape[j] * scale;
-			vertices[j+1] = data[i].shape[j+1] * scale; 
-		}newData.push({shape : vertices});
-	}var item = {};
-	item[shapeKey] = newData;game.load.physics(newPhysicsKey, '', item);
-	//
+
+	var newData = [];										//array to hold new polygon
+
+	var data = this.game.cache.getPhysicsData(originalPhysicsKey, shapeKey); //grab physics file
+
+	
+	for (var i = 0; i < data.length; i++) {					//iterate through all shapes in polygon
+		var vertices = [];									//new array to hold vertex data
+		for (var j = 0; j < data[i].shape.length; j += 2) {	//iterate through all vertices
+			vertices[j] = data[i].shape[j] * scale;			//scale
+			vertices[j+1] = data[i].shape[j+1] * scale; 	//scale
+		}
+		newData.push({shape : vertices}); 					//set new shape
+	}
+	var item = {};											//new object to hold json data
+	item[shapeKey] = newData;								//set adjusted polygon
+	game.load.physics(newPhysicsKey, '', item);				//create new key in cashe
+
 	//debugPolygon(newPhysicsKey, shapeKey);
 }
+/**
+*Movement button callbacks
+*Used for running acceleration curve
+*/
+startRun = function(){
 
+}
+stopRun = function(){
+
+}
 /**
 *Mechanic functions
 */
