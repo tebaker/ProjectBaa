475eb5e7e6ddb3a7a14271d70804077b907854ca
Bropocalypse <jalafond@ucsc.edu>
2017-06-13 20:58:40 -0700

    Create EndWin.js
    added sound trigger and edited text for win state. also made button press for advancement

--- game/js/states/EndWin.js
+++ game/js/states/EndWin.js
@@ -1,14 +1,27 @@
 'use strict';
 var EndWin = function(game) {};
+this.key;
 EndWin.prototype =
 {
+	preload: function()
+	{
+		game.load.audio('success',['assets/sound/success.mp3','assets/sound/success.ogg']);
+	},
+
 	create: function()
 	{
-		this.add.text(this.camera.x, this.camera.y, 'You win!', {fontSize: '32px', fill: 'white'});
+	var success = game.add.audio("success", 1, false);
+	success.play();
+	this.add.text(this.camera.x, this.camera.y, 'SUCCESS', {fontSize: '56px', fill: 'white'});
+	this.add.text(690, 360, 'Thank you for your child. . .Press Enter to repeat this process. . .', {fontSize: '48px', fill: 'white'});
+	this.key = this.input.keyboard.addKey(Phaser.KeyCode.ENTER);
 	},
 
 	update: function()
 	{
-    this.game.state.start('MainMenu');
+  	if (this.key.justPressed())
+		{
+		this.game.state.start('MainMenu');
+		}
 	},
 };
